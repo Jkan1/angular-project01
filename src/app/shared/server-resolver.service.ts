bd221a4540864a92c09d6f92bae832429185f2dd
Kanishka Joshi <kan.joshi.1@gmail.com>
2020-06-09 21:30:05 +0530

    Route Guards

---
+++ src/app/shared/server-resolver.service.ts
@@ -0,0 +1,20 @@
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+
+interface Server {
+    id: number;
+    name: string;
+    status: string;
+}
+
+
+export class ServerResolver implements Resolve<Server> {
+
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+        Observable<Server> | Promise<Server> | Server {
+        return
+    }
+
+}
