16d9056dd3df299a607fee0ad103e15fcb055c94
JKan1 <kan.joshi.1@gmail.com>
2023-10-18 00:58:25 +0530

    Added new RecipeFloater component

--- src/app/recipes/recipe.module.ts
+++ src/app/recipes/recipe.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RecipesComponent } from './recipes.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeFloaterComponent } from './recipe-floater/recipe-floater.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
@@ -15,6 +16,7 @@ import { SharedModule } from '../shared/shared.module';
         RecipesComponent,
         RecipeDetailComponent,
         RecipeListComponent,
+        RecipeFloaterComponent,
         RecipeItemComponent,
         RecipeStartComponent,
         RecipeEditComponent
