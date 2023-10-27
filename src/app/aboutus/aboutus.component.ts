81527ac59b5f2e77f374ae5cdf6df8b972aba631
JKan1 <kan.joshi.1@gmail.com>
2023-10-28 03:16:59 +0530

    Added Fade-in Animations to AboutUs & RecipeBanner Components

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,21 +1,43 @@
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
-import { Store } from '@ngrx/store';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 
+const animations = [
+  trigger('fadeAnimation', [
+    state('', style({
+      "opacity": 0
+    })),
+    state('loading', style({
+      "opacity": 1
+    })),
+    transition('* => loading', [
+      style({
+        "opacity": 0
+      }),
+      animate(1000)
+    ])
+  ])
+]
 @Component({
   selector: 'app-about-us',
   templateUrl: './aboutus.component.html',
-  styleUrls: ['./aboutus.component.css']
+  styleUrls: ['./aboutus.component.css'],
+  animations: animations
 })
-export class AboutUsComponent {
+export class AboutUsComponent implements OnInit {
 
   @ViewChild('f', { static: false }) form1: NgForm;
 
   subscription: Subscription;
   editMode = false;
   editedItem: Ingredient;
+  animationState = '';
 
   constructor() { }
+
+  ngOnInit() {
+    this.animationState = 'loading';
+  }
 }
--- src/app/recipes/recipe-banner/recipe-banner.component.ts
+++ src/app/recipes/recipe-banner/recipe-banner.component.ts
@@ -1,14 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate, state } from '@angular/animations';
+
+const animations = [
+  trigger('fadeAnimation', [
+    state('', style({
+      "opacity": 0
+    })),
+    state('loaded', style({
+      "opacity": 1
+    })),
+    transition('* => loaded', [
+      style({
+        "opacity": 0
+      }),
+      animate(500)
+    ])
+  ])
+]
 
 @Component({
   selector: 'app-recipe-banner',
-  templateUrl: './recipe-banner.component.html'
+  templateUrl: './recipe-banner.component.html',
+  animations: animations
 })
 export class RecipeBannerComponent implements OnInit {
+
+  animationState = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    this.animationState = 'loaded';
+  }
+
+  onImageLoad(imgIndex: number) {
+    console.log(imgIndex);
+    if (imgIndex == 1) this.animationState = 'loaded';
+    if (imgIndex == 2) this.animationState = 'loaded';
+    if (imgIndex == 3) this.animationState = 'loaded';
   }
 
 }
