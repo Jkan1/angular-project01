bdc3b28aabc0f11bdd36494bcfe391f42c8eea19
JKan1 <kan.joshi.1@gmail.com>
2024-01-25 02:07:11 +0530

    Updated environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
-  firebaseApiKey: 'AIzaSyC0rQyoGwBIhqgSyM_BOTGz_UN6jO8hCQ8',
+  firebaseApiKey: 'AIzaSyCIIYdJu1-O4rBzksOfY7zaN9gxxw3rupg',
   firebaseSignupUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
   firebaseSignInWithPasswordUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
   firebaseSetUserProfileUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:update?key=',
   firebaseGetUserProfileUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=',
   sendEmailVerificationUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=',
   veryUserIdentityUrl:'https://identitytoolkit.googleapis.com/v1/accounts:update?key=',
-  dataBaseUrl: 'https://recipe-d6253.firebaseio.com/recipes.json',
+  dataBaseUrl: 'https://list-o-chef-default-rtdb.asia-southeast1.firebasedatabase.app/data.json',
+  inviteCodeUrl: 'https://list-o-chef-default-rtdb.asia-southeast1.firebasedatabase.app/invite_codes/',
   sampleProfileImage:'https://www.alchinlong.com/wp-content/uploads/2015/09/sample-profile.png'
 };
