5bf31d5e4723bd512a7b3a2bd88f60e45dd0789a
JKan1 <kan.joshi.1@gmail.com>
2024-01-25 00:12:47 +0530

    Minor cleanup fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseApiKey: 'AIzaSyC0rQyoGwBIhqgSyM_BOTGz_UN6jO8hCQ8',
+  firebaseApiKey: 'AIzaSyCIIYdJu1-O4rBzksOfY7zaN9gxxw3rupg',
   firebaseSignupUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
   firebaseSignInWithPasswordUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
   firebaseSetUserProfileUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:update?key=',
@@ -12,6 +12,7 @@ export const environment = {
   sendEmailVerificationUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=',
   veryUserIdentityUrl:'https://identitytoolkit.googleapis.com/v1/accounts:update?key=',
   dataBaseUrl: 'https://list-o-chef-default-rtdb.asia-southeast1.firebasedatabase.app/data.json',
+  inviteCodeUrl: 'https://list-o-chef-default-rtdb.asia-southeast1.firebasedatabase.app/invite_codes/',
   sampleProfileImage:'https://www.alchinlong.com/wp-content/uploads/2015/09/sample-profile.png'
 };
 
